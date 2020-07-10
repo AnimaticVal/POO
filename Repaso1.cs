03cd205be378f486b5ea90b36386ba0726793efe
AnimaticVal <60115544+AnimaticVal@users.noreply.github.com>
2020-07-10 00:04:51 -0500

    Add files via upload

---
+++ Repaso1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 1;
+            int a = 0 ;
+            int b = 1;
+            int limite= 0;
+            Console.WriteLine("Ingrese el número limmite");
+            limite = int.Parse(Console.ReadLine());
+             
+            for (int i = 0; i < limite; i++) {
+                Console.WriteLine(suma);
+                suma = a + b;
+                a = b;
+                b = suma;
+               
+                if (suma > 1000) {
+                    break; 
+                }
+            }
+        }
+    }
+}
