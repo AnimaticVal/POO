5e280e67cda8e72ee354f58ea088ab416a99ffdd
AnimaticVal <60115544+AnimaticVal@users.noreply.github.com>
2020-07-10 10:21:41 -0500

    Add files via upload

---
+++ Repaso1DoWhile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 1;
+            int a = 0;
+            int b = 1;
+
+
+            do
+            {
+                Console.WriteLine(suma);
+                suma = a + b;
+                a = b;
+                b = suma;
+            } while (suma < 10000);
+              
+            
+
+        }
+    }
+}
