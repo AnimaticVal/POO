a1015da06fd69b9f8c9d27498d301df92978e6f7
AnimaticVal <60115544+AnimaticVal@users.noreply.github.com>
2020-07-20 22:04:02 -0500

    Add files via upload

---
+++ CiclosAnidados.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosAnidados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int i, j, k = 1;
+            for (i = 1; i <= 3; i++)
+            {
+                for (j = 1; j <= 3; j++)
+                {
+
+                    Console.WriteLine(k + "" + i + "" + j);
+
+                    k++;
+                }
+
+            }*/
+
+            int i;
+            for (i = 25 ; i <= 250; i++)
+            {
+               String Num = i.ToString();
+                if (Num.EndsWith("6")) {
+                    Console.WriteLine(Num);
+  
+                }
+                
+            }
+
+        }
+    }
+}
